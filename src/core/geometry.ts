c6288c42fb8928364e26854c76c539aad8654a3c
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-02-10 22:35:15 +0100

    feat(Geometry): Add support for modelMatrix

--- src/core/geometry.ts
+++ src/core/geometry.ts
@@ -1,11 +1,27 @@
+import { mat4 } from 'gl-matrix'
+
 import type { WebGLContext } from '../ts-types'
 
 import VAO from './vao'
 import { createBuffer, createIndexBuffer, getExtension } from '../utils/gl-utils'
+import { vec3 } from 'gl-matrix'
 
 export default class Geometry {
   public attributes = new Map()
   public vertexCount = 0
+  public modelMatrix = mat4.create()
+    
+  #position = [0, 0, 0]
+  #positionVec3 = vec3.create()
+
+  #scale: [number, number, number] = [1, 1, 1]
+  #scaleVec3 = vec3.create()
+
+  #rotationAxis = [0, 0, 0]
+  #rotationAxisVec3 = vec3.create()
+  #rotationAngle = 0
+
+
 
   #gl
   #vao
@@ -19,10 +35,39 @@ export default class Geometry {
     this.#vao = new VAO(gl)
 
     this.#instancedExtension = getExtension(gl, 'ANGLE_instanced_arrays')
+
+    vec3.set(this.#scaleVec3, ...this.#scale)
   }
 
   set instanceCount (count: number) {
     this.#instanceCount = count
+  }
+
+  setPosition ({ x = this.#position[0], y = this.#position[1], z = this.#position[2] }) {
+    this.#position = [x, y, z]
+    vec3.set(this.#positionVec3, x, y, z)
+    return this
+  }
+
+  setScale ({ x = this.#scale[0], y = this.#scale[1], z = this.#scale[2] }) {
+    this.#scale = [x, y, z]
+    vec3.set(this.#scaleVec3, x, y, z)
+    return this
+  }
+
+  setRotation ({ x = this.#rotationAxis[0], y = this.#rotationAxis[1], z = this.#rotationAxis[2] }, rotationAngle) {
+    this.#rotationAxis = [x, y, z]
+    vec3.set(this.#rotationAxisVec3, x, y, z)
+    this.#rotationAngle = rotationAngle
+    return this
+  }
+
+  updateModelMatrix () {
+    mat4.identity(this.modelMatrix)
+    mat4.scale(this.modelMatrix, this.modelMatrix, this.#scaleVec3)
+    mat4.rotate(this.modelMatrix, this.modelMatrix, this.#rotationAngle, this.#rotationAxisVec3)
+    mat4.translate(this.modelMatrix, this.modelMatrix, this.#positionVec3)
+    return this
   }
 
   bind () {
