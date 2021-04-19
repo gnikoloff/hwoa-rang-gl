1516293ed4d0051b2dba8421ff04280555f2441b
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-04-19 23:14:13 +0200

    fix(Examples): Point demo src links to correct github link

--- docs/examples/index.js
+++ docs/examples/index.js
@@ -41,14 +41,14 @@ function onHashChange() {
 
     sourceLinkBtn.setAttribute(
       'href',
-      `https://github.com/gnikoloff/hwoa-rang-gl/tree/main/docs/examples/examples-src/${exampleName}/index.js`,
+      `https://github.com/gnikoloff/hwoa-rang-gl/tree/main/docs/examples/src/${exampleName}/index.js`,
     )
   } else {
     const exampleName = 'particles-01'
     location.hash = exampleName
     sourceLinkBtn.setAttribute(
       'href',
-      `https://github.com/gnikoloff/hwoa-rang-gl/tree/main/docs/examples/examples-src/${exampleName}/index.js`,
+      `https://github.com/gnikoloff/hwoa-rang-gl/tree/main/docs/examples/src/${exampleName}/index.js`,
     )
   }
   if (appAside.classList.contains('visible')) {
