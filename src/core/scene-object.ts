0f865ca0d47f9d0e1fd527ee6f30a6ade32edcd7
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-02-10 17:29:00 +0100

    upgrade scene-object with more helper methods

--- src/core/scene-object.ts
+++ src/core/scene-object.ts
@@ -1,17 +1,12 @@
-import { mat4, ReadonlyMat4 } from 'gl-matrix'
+import { mat4 } from 'gl-matrix'
 import { uid } from 'uid'
-import { Transform } from './transform'
+import { Transform } from '..'
 
-/**
- * SceneObject that can have SceneObjects as children. Allows for proper scene graph.
- *
- * @public
- */
 export class SceneObject extends Transform {
-  protected renderable = false
-
   parentNode: SceneObject | null = null
-  children: SceneObject[] = []
+
+  protected _children: SceneObject[] = []
+  protected _visible = true
 
   worldMatrix = mat4.create()
   normalMatrix = mat4.create()
@@ -19,26 +14,60 @@ export class SceneObject extends Transform {
   uid = uid(9)
   name?: string
 
+  get visible() {
+    return this._visible
+  }
+
+  set visible(v: boolean) {
+    this._visible = v
+  }
+
+  get children(): SceneObject[] {
+    return this._children
+  }
+
+  get siblings(): SceneObject[] {
+    if (!this.parentNode) {
+      return []
+    }
+    return this.parentNode._children
+  }
+
   constructor(name: string | undefined = undefined) {
     super()
     this.name = name
   }
 
-  setParent = (parentNode: SceneObject | null = null): this => {
+  get levelIndex(): number {
+    let levelIndex = 0
+    let parentNode = this.parentNode
+    while (parentNode) {
+      levelIndex++
+      parentNode = parentNode.parentNode
+    }
+    return levelIndex
+  }
+
+  setParent(parentNode: SceneObject | null = null): this {
     if (this.parentNode) {
-      const idx = this.parentNode.children.indexOf(this)
+      const idx = this.parentNode._children.indexOf(this)
       if (idx >= 0) {
-        this.parentNode.children.splice(idx, 1)
+        this.parentNode._children.splice(idx, 1)
       }
     }
     if (parentNode) {
-      parentNode.children.push(this)
+      parentNode.addChild(this)
     }
     this.parentNode = parentNode
     return this
   }
 
-  updateWorldMatrix = (parentWorldMatrix: ReadonlyMat4 | null = null): this => {
+  addChild(childNode: SceneObject): this {
+    this._children.push(childNode)
+    return this
+  }
+
+  updateWorldMatrix(parentWorldMatrix: mat4 | null = null): this {
     if (this.shouldUpdate) {
       this.updateModelMatrix()
     }
@@ -49,15 +78,84 @@ export class SceneObject extends Transform {
     }
     mat4.invert(this.normalMatrix, this.worldMatrix)
     mat4.transpose(this.normalMatrix, this.normalMatrix)
-    this.children.forEach((child) => {
-      child.updateWorldMatrix(this.worldMatrix)
-    })
+    for (let i = 0; i < this._children.length; i++) {
+      this._children[i].updateWorldMatrix(this.worldMatrix)
+    }
+    // console.log(this.worldMatrix)
     return this
   }
 
-  traverseGraph = (callback, node: SceneObject = this): this => {
-    callback(node)
-    this.children.forEach((child) => child.traverseGraph(callback))
-    return this
+  traverse(
+    callback: (node: SceneObject, depthLevel: number) => void,
+    depth = 0,
+  ): void {
+    callback(this, depth)
+    depth++
+    for (let i = 0; i < this._children.length; i++) {
+      const child = this._children[i]
+      child.traverse(callback, depth)
+    }
+  }
+
+  findChild(
+    callback: (node: SceneObject) => SceneObject | boolean | null,
+  ): SceneObject | null {
+    if (callback(this)) {
+      return this
+    }
+    let outNode: SceneObject | null = null
+    for (let i = 0; i < this._children.length; i++) {
+      const child = this._children[i]
+      if ((outNode = child.findChild(callback))) {
+        break
+      }
+    }
+    return outNode
+  }
+
+  findChildByName(name: string): SceneObject | null {
+    if (this.name === name) {
+      return this
+    }
+    let outNode: SceneObject | null = null
+    for (let i = 0; i < this._children.length; i++) {
+      const child = this._children[i]
+      if ((outNode = child.findChildByName(name))) {
+        break
+      }
+    }
+    return outNode
+  }
+
+  findParent(
+    callback: (node: SceneObject) => SceneObject | boolean | null,
+  ): SceneObject | null {
+    if (callback(this)) {
+      return this
+    }
+    let outNode: SceneObject | null = null
+    let parentNode = this.parentNode
+    while (parentNode) {
+      if ((outNode = parentNode.findParent(callback))) {
+        break
+      }
+      parentNode = parentNode?.parentNode
+    }
+    return outNode
+  }
+
+  findParentByName(name: string): SceneObject | null {
+    if (this.name === name) {
+      return this
+    }
+    let outNode: SceneObject | null = null
+    let parentNode = this.parentNode
+    while (parentNode) {
+      if ((outNode = parentNode.findParentByName(name))) {
+        break
+      }
+      parentNode = parentNode?.parentNode
+    }
+    return outNode
   }
 }
